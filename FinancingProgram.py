a61c36824fdb96df8512ac6fbbb33d5411f8b145
Neil Smith <s3nt1n3lz14@yahoo.co.uk>
2015-07-24 13:31:01 +0100

    New Commit
    This is the comment of the new commit

---
+++ FinancingProgram.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 24 13:23:30 2015
+
+@author: Neil
+"""
+
+#This is a personal financing program. It keeps track of your spending and helps to manage expenditure for you by setting goals and prioritising
+#them.
